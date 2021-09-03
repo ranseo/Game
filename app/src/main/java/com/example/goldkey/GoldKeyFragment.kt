a3cb0720b1ad2a56de18c46dc87fb8fa3e623c2c
ranseo <slcksseo@gmail.com>
2021-09-03 21:46:14 +0900

    "커플카드로 재탄생"

--- app/src/main/java/com/example/goldkey/GoldKeyFragment.kt
+++ app/src/main/java/com/example/goldkey/GoldKeyFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AnimationUtils
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.get
@@ -27,6 +28,15 @@ class GoldKeyFragment : Fragment() {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_gold_key, container, false)
         binding.lifecycleOwner = viewLifecycleOwner
         binding.viewModel = goldKeyViewModel
+
+        val anim = AnimationUtils.loadAnimation(context, R.anim.wave)
+        val anim2 = AnimationUtils.loadAnimation(context, R.anim.wave_ver2)
+
+        binding.heart1.startAnimation(anim)
+        binding.heart2.startAnimation(anim)
+        binding.heart3.startAnimation(anim)
+
+        binding.pickCardTv.startAnimation(anim2)
 
 
         return binding.root
--- app/src/main/java/com/example/goldkey/util/BindingAdapter.kt
+++ app/src/main/java/com/example/goldkey/util/BindingAdapter.kt
@@ -3,25 +3,51 @@ package com.example.goldkey.util
 import android.view.View
 import android.view.View.INVISIBLE
 import android.view.View.VISIBLE
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.databinding.BindingAdapter
+import com.example.goldkey.R
 import org.w3c.dom.Text
 
-@BindingAdapter("app:visibilityToggle")
-fun changeVisiblityForImageView(view: ImageView, toggle: Boolean){
-    if(toggle)
-        view.visibility = VISIBLE
-    else
-        view.visibility = INVISIBLE
+//@BindingAdapter("app:visibilityToggle")
+//fun changeVisiblityForImageView(view: ImageView, toggle: Boolean){
+//    if(toggle)
+//        view.visibility = VISIBLE
+//    else
+//        view.visibility = INVISIBLE
+//}
+//
+//@BindingAdapter("app:visibilityToggle")
+//fun changeVisiblityForTextView(view: TextView, toggle: Boolean){
+//    if(!toggle)
+//        view.visibility = VISIBLE
+//    else
+//        view.visibility = INVISIBLE
+//}
+
+@BindingAdapter(value = ["app:visibilityToggle","android:flag"], requireAll =true)
+fun changeVisiblityForConstraintLayout(view: ConstraintLayout, toggle: Boolean, flag:Boolean){
+
+    if(flag) {
+        if (toggle)
+            view.visibility = VISIBLE
+        else
+            view.visibility = INVISIBLE
+    } else {
+        if(!toggle)
+            view.visibility = VISIBLE
+        else
+            view.visibility = INVISIBLE
+    }
 }
 
-@BindingAdapter("app:visibilityToggle")
-fun changeVisiblityForTextView(view: TextView, toggle: Boolean){
-    if(!toggle)
-        view.visibility = VISIBLE
-    else
-        view.visibility = INVISIBLE
+@BindingAdapter("android:setAnimation")
+fun setAniamtion(view:ImageView, anim: Animation){
+
+    view.animation= anim
 }
 
 
